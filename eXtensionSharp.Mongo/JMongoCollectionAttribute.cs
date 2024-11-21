e3a4308e78ce51bd63b35c8214bdd2b3dd9146b9
seokwonhong <h20913@gmail.com>
2024-11-22 00:44:06 +0900

    first

---
+++ eXtensionSharp.Mongo/JMongoCollectionAttribute.cs
@@ -0,0 +1,14 @@
+﻿namespace eXtensionSharp.Mongo;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class JMongoCollectionAttribute : Attribute
+{
+    public string DatabaseName { get; }
+    public string CollectionName { get; }
+
+    public JMongoCollectionAttribute(string databaseName, string collectionName)
+    {
+        DatabaseName = databaseName;
+        CollectionName = collectionName;
+    }
+}
