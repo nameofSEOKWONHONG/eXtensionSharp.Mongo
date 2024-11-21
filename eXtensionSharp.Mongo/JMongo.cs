b59fb750758440005a0471c8210bf0690b156449
seokwonhong <h20913@gmail.com>
2024-11-22 00:44:06 +0900

    first

---
+++ eXtensionSharp.Mongo/JMongo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using MongoDB.Driver;
+
+namespace eXtensionSharp.Mongo;
+
+public class JMongo<T>
+    where T : class
+{
+    private JMongo(string databaseName, string collectionName, IMongoClient client)
+    {
+        _databaseName = databaseName;
+        _collectionName = collectionName;
+        _client = client;
+    }
+    
+    private IMongoClient _client;
+    private string _databaseName;
+    private string _collectionName;
+
+    public IMongoCollection<T> GetCollection()
+    {
+        var db = _client.GetDatabase(_databaseName);
+        return db.GetCollection<T>(_collectionName);
+    }
+
+    public static JMongo<T> Create(IMongoClient client)
+    {
+        var attribute = typeof(T).GetCustomAttribute<JMongoCollectionAttribute>();
+        if (attribute == null) throw new Exception("Not define MongoCollectionAttribute");
+        return new JMongo<T>(attribute!.DatabaseName, attribute.CollectionName, client);
+    }
+}
---
+++ test/eXtensionSharp.Mongo.Test/JMongoTest.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+namespace eXtensionSharp.Mongo.Test;
+
+public class Tests
+{
+    private readonly string _mongoConnectionString = "mongodb://localhost:27017";
+
+    
+    [Test]
+    public void jmongo_test()
+    {
+        var client = new MongoClient(_mongoConnectionString);
+        var collection = JMongo<Sample>.Create(client).GetCollection();
+        var item = new Sample { Name = "test", Age = 100 };
+        collection.InsertOne(item);
+        Assert.That(item.Id, Is.Not.Null);
+    }
+}
+
+[JMongoCollection("sample", "demo")]
+public class Sample
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    [BsonElement("id")]
+    public String Id { get; set; }
+    
+    [BsonElement("name")]
+    public string Name { get; set; }
+    
+    [BsonElement("age")]
+    public int Age { get; set; }
+}
